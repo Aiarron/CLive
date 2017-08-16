b08615b2ce8f6ccadb1f7db9259129615d3f65f2
liaohongdong <liaodoho@gmail.com>
2017-08-16 18:27:16 +0800

    直播间推送商品列表
    推送商品列表

--- src/app/live/live-room/live-room.component.ts
+++ src/app/live/live-room/live-room.component.ts
@@ -5,41 +5,56 @@ declare var Swiper: any;
 declare var prismplayer: any;
 
 @Component({
-  selector: 'app-live-room',
-  templateUrl: './live-room.component.html',
-  styleUrls: ['./live-room.component.scss']
+    selector: 'app-live-room',
+    templateUrl: './live-room.component.html',
+    styleUrls: ['./live-room.component.scss']
 })
 export class LiveRoomComponent implements OnInit {
-  public player;
-  public cover;
-  public source;
-  public barrageSwitch: boolean = false;
+    public player;
+    public cover;
+    public source;
+    public barrageSwitch: boolean = false;
+    public textAreNum: number = 0;
 
-  constructor() { }
+    public itemsPerPage: number = 9;
+    public totalRecords: number = 9;
+    public currentPage: number = 1;
+    public offset: number = 0;
+    public end: number = 0;
 
-  ngOnInit() {
+    constructor() { }
 
-    this.player = new prismplayer({
-      id: "J_prismPlayer", // 容器id
-      // source: "http://pili-live-hls.www.autoclub.com.cn/diwei-live/test.m3u8",
-      source: this.source,
-      autoplay: true,    //自动播放：否
-      width: "100%",       // 播放器宽度
-      height: "inherit",      // 播放器高度
-      isLive: true,
-      preload: true,
-      cover: this.cover
-    });
+    ngOnInit() {
 
-    // 监听播放器的pause事件
-    this.player.on("pause", function () {
-      console.log("播放器暂停啦！");
-    });
+        this.player = new prismplayer({
+            id: "J_prismPlayer", // 容器id
+            // source: "http://pili-live-hls.www.autoclub.com.cn/diwei-live/test.m3u8",
+            source: this.source,
+            autoplay: true,    //自动播放：否
+            width: "100%",       // 播放器宽度
+            height: "inherit",      // 播放器高度
+            isLive: true,
+            preload: true,
+            cover: this.cover
+        });
 
-  }
+        // 监听播放器的pause事件
+        this.player.on("pause", function() {
+            console.log("播放器暂停啦！");
+        });
 
-  getBarrage(data){ // 弹幕开关
-    this.barrageSwitch = data;
-  }
+    }
+
+    getBarrage(data) { // 弹幕开关
+        this.barrageSwitch = data;
+    }
+
+    saveBarrageNum(value) {
+        this.textAreNum = value.length;
+    }
+
+    sendBarrage(value) {
+        console.log(value);
+    }
 
 }
